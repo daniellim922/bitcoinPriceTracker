890eb347a8a51c09e928d3f29a29c3cc8f2f25ba
daniellim922 <daniellim922@gmail.com>
2022-01-02 15:42:47 +0800

    append onto page~

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fetchBitcoinPrice = async () => {
 	try {
 		const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd');
-		console.log(res.data.ticker.price);
+		let bitcoinPrice = document.querySelector('#bitcoinPrice');
+		btcPrice = parseFloat(res.data.ticker.price).toFixed(2);
+		bitcoinPrice.innerText = `$${btcPrice}`;
 	} catch (e) {
 		console.log('ERROR', e);
 	}
